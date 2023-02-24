8e22f23d71e9f6bc6f92567ad330b554bcd2bf51
Hasaranga <rvndhasaranga@gmail.com>
2023-02-24 12:49:41 +0530

    create mern app and create header,footer,layout of components

---
+++ gems-and-jewellery-e-commerce-mern-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+  <h1>Hello from app</h1>
+  );
+}
+
+export default App;
